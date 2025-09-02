68b1714a4596d1f044575aa9ebdf2993900d7a22
Артём Миронов <mrtxee@gmail.com>
2025-09-02 19:13:35 +0300

    docker-compose up

--- src/main/java/mr/xe/wto/app/controller/TemperatureController.java
+++ src/main/java/mr/xe/wto/app/controller/TemperatureController.java
@@ -2,7 +2,8 @@ package mr.xe.wto.app.controller;
 
 import java.util.Random;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TemperatureController {
 
   private static final Random RANDOM = new Random();
-  @GetMapping("temperature")
+
+  @RequestMapping(
+      value = {"/temperature", "/temperature/**"},
+      method = {RequestMethod.GET, RequestMethod.PATCH}
+  )
   public String getTemperature() {
     return String.valueOf(RANDOM.nextInt(-50, 50));
   }
